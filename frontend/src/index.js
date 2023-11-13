f159fc95ec25e19e78a87f2698a2514a283520ed
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2023-11-13 12:21:06 +0530

    landing page started

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,11 @@ import UserProvider from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
     <Router>
       <UserProvider>
         <App />
       </UserProvider>
     </Router>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
