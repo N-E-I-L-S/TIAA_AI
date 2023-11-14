d90e7d5987860bad8c15c76a67e0f328a3c376e1
Yash Thakar <90375840+yashyaks@users.noreply.github.com>
2023-11-14 16:13:31 +0530

    landing signin and navbar added

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,60 +4,54 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Navbar from '../components/Navbar'
 
 export default function Landing() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <div className="bg-white">
-      <Navbar/>
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+    <>
+    <Navbar/>
+      <div className="relative isolate px-6 lg:px-8">
         <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
+          <div className=" sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 font-body text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Retirement Business Challenge....{' '}
+              <a href="#" className="font-semibold text-blue-900">
+                <span className="absolute inset-0 font-body" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <span className="text-8xl font-title tracking-tight text-blue-100 sm:text-6xl">
+              Hire2
+            </span>
+            <span className="text-8xl font-title tracking-tight text-blue-900 sm:text-6xl">
+              Retire
+            </span>
+            <p className="mt-6 text-lg font-body leading-8 text-gray-600">
+              Transform your employee lifecycle with AI-driven retirement solutions for employees, tailored annuity options, and organizational enhancements.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-blue-100 px-3.5 py-2.5 text-md font-body text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a href="#" className="text-md font-body leading-6 text-black-900">
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
         </div>
-        <div
+        {/* <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
-        </div>
+        </div> */}
       </div>
-    </div>
+    </>
   )
 }
